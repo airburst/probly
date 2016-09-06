05b8778970860d0742d5207a8b45fa5a8142c0b5
airburst <mark@fairhursts.net>
2016-09-06 11:43:48 +0100

    Moved firebase init into actions/feedback

--- app/actions/feedback.js
+++ app/actions/feedback.js
@@ -1,8 +1,32 @@
+import { store } from '../index';
+const firebase = require('firebase');
+
 export const SET_FEEDBACK = 'SET_FEEDBACK';
+
+const config = {
+  apiKey: 'AIzaSyDzuk7Y6AFa_mVJ821wCv48QtSeXPzyKSc',
+  authDomain: 'probly-36ae1.firebaseapp.com',
+  databaseURL: 'https://probly-36ae1.firebaseio.com',
+  storageBucket: 'probly-36ae1.appspot.com',
+};
+const Firebase = firebase.initializeApp(config);
+const fireDB = Firebase.database().ref('mylife/4-0-0');
 
 export function setFeedback(feedback) {
   return {
     type: SET_FEEDBACK,
     payload: feedback
   };
+}
+
+export function initialise() {
+  fireDB.on('value', (data) => {
+    const feedback = [];
+    data.forEach((row) => {
+      const item = Object.assign({}, row.val());
+      item.key = row.key;
+      feedback.push(item);
+    });
+    store.dispatch(setFeedback(feedback));
+  });
 }
--- app/index.js
+++ app/index.js
@@ -7,31 +7,34 @@ import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
 import * as FeedbackActions from './actions/feedback';
-const firebase = require('firebase');
+//const firebase = require('firebase');
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Setup Firebase
-const config = {
-  apiKey: 'AIzaSyDzuk7Y6AFa_mVJ821wCv48QtSeXPzyKSc',
-  authDomain: 'probly-36ae1.firebaseapp.com',
-  databaseURL: 'https://probly-36ae1.firebaseio.com',
-  storageBucket: 'probly-36ae1.appspot.com',
-};
-const fb = firebase.initializeApp(config);
-const ref = fb.database().ref('mylife/4-0-0');
+// const config = {
+//   apiKey: 'AIzaSyDzuk7Y6AFa_mVJ821wCv48QtSeXPzyKSc',
+//   authDomain: 'probly-36ae1.firebaseapp.com',
+//   databaseURL: 'https://probly-36ae1.firebaseio.com',
+//   storageBucket: 'probly-36ae1.appspot.com',
+// };
+// export const Firebase = firebase.initializeApp(config);
+// console.log(Firebase);
+//const ref = Firebase.database().ref('mylife/4-0-0');
 
-const store = configureStore();
+export const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
-ref.on('value', (data) => {
-  const feedback = [];
-  data.forEach((row) => {
-    const item = Object.assign({}, row.val());
-    item.key = row.key;
-    feedback.push(item);
-  });
-  store.dispatch(FeedbackActions.setFeedback(feedback));
-});
+// ref.on('value', (data) => {
+//   const feedback = [];
+//   data.forEach((row) => {
+//     const item = Object.assign({}, row.val());
+//     item.key = row.key;
+//     feedback.push(item);
+//   });
+//   store.dispatch(FeedbackActions.setFeedback(feedback));
+// });
+
+FeedbackActions.initialise();
 
 // Touch Tap Plugin
 injectTapEventPlugin();
